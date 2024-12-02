1c9d6885cd4288b35986a2afd9d713df4dca94ba
Ntobeko Sikithi <ntobekosikithi@woolworths.co.za>
2024-12-02 17:37:48 +0200

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by ns90840 on 2024/12/02.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
